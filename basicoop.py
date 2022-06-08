fcd40cc4febc997413659eed47d809d8dca3c65b
onze <slapexs@gmail.com>
2022-06-08 12:56:12 +0700

    Create class and object

---
+++ basicoop.py
@@ -0,0 +1,18 @@
+'''
+    ข้อมูลพนักงาน -> ชื่อ, เงินเดือน
+'''
+
+'''
+    สร้างคลาส
+    class Classname:
+        statement
+'''
+class Employee:
+    pass
+
+
+'''
+    สร้าง Object
+    variable = class()
+'''
+em1 = Employee()
